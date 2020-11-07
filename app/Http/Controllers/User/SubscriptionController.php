c3671184f41d1c5cb296c3aba6fc3aa9405f83ce
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-11-07 19:13:03 +0900

    subscription修正

--- app/Http/Controllers/User/SubscriptionController.php
+++ app/Http/Controllers/User/SubscriptionController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('user.subscription.index');
+        $user = $request->user();
+
+        return view('user.subscription.index')->with([
+            'intent' => $user->createSetupIntent(),
+        ]);
     }
 }
